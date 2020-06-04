96e326f5955fdde307da2d528b1937cebbfbf413
Radoslav Pitonak <rado.pitonak@gmail.com>
2020-06-04 19:03:40 +0200

    Bump version: 0.1.0 → 0.1.1

--- geojson_length/__init__.py
+++ geojson_length/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Zaitra"""
 __email__ = "info@zaitra.io"
-__version__ = "0.1.0"
+__version__ = "__version__ = '0.1.1'"
 
 from geojson_length.geojson_length import calculate_distance, Unit
 
